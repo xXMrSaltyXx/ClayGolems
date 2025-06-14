28f34220f53e1611623658e48573f0391237219c
MrSalty <mister.salty@tutanota.com>
2025-06-15 00:02:31 +0200

    initialize ClayGolems mod with Gradle setup and necessary configuration files

---
+++ src/main/java/com/mrsalty/claygolems/ClayGolems.java
@@ -0,0 +1,10 @@
+package com.mrsalty.claygolems;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ClayGolems implements ModInitializer {
+
+  @Override
+  public void onInitialize() {
+  }
+}
---
+++ src/main/java/com/mrsalty/claygolems/ClayGolemsClient.java
@@ -0,0 +1,9 @@
+package com.mrsalty.claygolems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClayGolemsClient implements ClientModInitializer {
+
+  @Override
+  public void onInitializeClient() {}
+}
